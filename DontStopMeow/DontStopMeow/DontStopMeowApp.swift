9dfefa2981ab5f901c1502e33fb588698efcb5a2
JAEHYUN GO <gojaehyeonstudio@gmail.com>
2025-07-16 17:31:43 +0900

    feat: 프로젝트 생성

---
+++ DontStopMeow/DontStopMeow/DontStopMeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontStopMeowApp.swift
+//  DontStopMeow
+//
+//  Created by Gojaehyun on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DontStopMeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DontStopMeow/DontStopMeowTests/DontStopMeowTests.swift
@@ -0,0 +1,17 @@
+//
+//  DontStopMeowTests.swift
+//  DontStopMeowTests
+//
+//  Created by Gojaehyun on 7/16/25.
+//
+
+import Testing
+@testable import DontStopMeow
+
+struct DontStopMeowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
